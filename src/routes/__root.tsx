07d797c1c8b3c5e448ab33b167ba74d2e712ff71
Luís Tavares <luistavares10@outlook.pt>
2025-04-30 12:16:10 +0100

    fix unauthenticated preload

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,6 +3,7 @@ import {
   createRootRouteWithContext,
   Link,
   Outlet,
+  useLocation,
 } from "@tanstack/react-router";
 
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
@@ -13,22 +14,26 @@ import { Footer } from "@/components/Footer";
 export const Route = createRootRouteWithContext<{
   accessToken: string | null;
 }>()({
-  component: () => (
-    <StackProvider app={stackClientApp}>
-      <StackTheme>
-        <div className="min-h-screen flex flex-col">
-          <main className="max-w-2xl mx-auto p-4 mt-14 flex-1">
-            <header className="mb-8">
-              <Link to="/">
-                <h3>note.</h3>
-              </Link>
-            </header>
-            <Outlet />
-          </main>
-          <Footer />
-        </div>
-        <TanStackRouterDevtools />
-      </StackTheme>
-    </StackProvider>
-  ),
+  component: () => {
+    const location = useLocation();
+    const shouldPreload = !location.pathname.includes("/handler/");
+    return (
+      <StackProvider app={stackClientApp}>
+        <StackTheme>
+          <div className="min-h-screen flex flex-col">
+            <main className="min-w-0 w-full max-w-2xl mx-auto p-4 mt-14 flex-1">
+              <header className="mb-8">
+                <Link to="/" preload={shouldPreload ? "intent" : false}>
+                  <h3>note.</h3>
+                </Link>
+              </header>
+              <Outlet />
+            </main>
+            <Footer />
+          </div>
+          <TanStackRouterDevtools />
+        </StackTheme>
+      </StackProvider>
+    );
+  },
 });
